8d64b09def46e6ed9f6dfca439d087198c0b13a1
Cristiano <cristiano.tarconi@gmail.com>
2021-03-25 17:22:13 +0100

    slug rep check - tables relation - fake posts seed

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Post;
+use App\User;
 
 class PostSeeder extends Seeder
 {
@@ -17,8 +18,32 @@ class PostSeeder extends Seeder
         for ($i = 0; $i < 10; $i++){
             $newPost = new Post();
             $newPost->title = $faker->sentence(4);
-            $newPost->slug = Str::slug($newPost->title, '-');
             $newPost->content = $faker->text(500);
+
+            $userCount = Count(User::all()->toArray());
+            $newPost->user_id = rand(1, $userCount);
+            
+            // salvo lo slug in una variabile
+            $slug = Str::slug($newPost->title);
+
+            // Conservo slug iniziale in una var così da non farlo cambiare in caso di ripetizioni
+            $slugIniziale = $slug;
+
+            // controllo se lo slug è presente
+            $postPresente = Post::where('slug', $slug)->first();
+
+            // Contatore per aggiunta numero a slug ripetuto
+            $contatore = 1;
+
+            // Ciclo per aggiungere num e incrementare contatore per eventuali ripetizioni successive
+            while ($postPresente) {
+                $slug = $slugIniziale . '-' .$contatore;
+                $postPresente = Post::where('slug', $slug)->first();
+                $contatore++;
+            }
+
+            // Aggiungo slug creato nel ciclo sopra in tabella 
+            $newPost->slug = $slug;
             
             $newPost->save();
         }
